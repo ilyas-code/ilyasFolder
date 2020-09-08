300fa0173c81fbf7fd7efc5f7fe71ca255ef1adb
Glitch (cooked-purrfect-skink) <none>
2020-09-08 14:04:14 +0000

    🏠😽 Updated with Glitch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
-
-function Header(props){
-    return(
-      
-        <div className="head-1">
-        <h1>Take your Quiz </h1>
-        </div> 
-      
-    )
+function Header(props) {
+  return (
+    <div className="head-1">
+      <h1>Take your Quiz </h1>
+    </div>
+  );
 }
 
 export default Header;
