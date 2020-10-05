23b1b3a7b7d758f3140b9272992ae0e80ca36484
ilyas-code <ilyas515253@gmail.com>
2020-10-05 20:08:12 +0530

    Updates

---
+++ precache-manifest.c3a83fdef1c61146afcde11a78822e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2738dde145f69b36982708822534211",
+    "url": "/ilyas-code/ilyasFolder.git/index.html"
+  },
+  {
+    "revision": "c510dbe71015eb11c24e",
+    "url": "/ilyas-code/ilyasFolder.git/static/css/main.2fd18e40.chunk.css"
+  },
+  {
+    "revision": "7cd13ba776e1784dfff4",
+    "url": "/ilyas-code/ilyasFolder.git/static/js/2.92e3acc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ilyas-code/ilyasFolder.git/static/js/2.92e3acc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c510dbe71015eb11c24e",
+    "url": "/ilyas-code/ilyasFolder.git/static/js/main.2f17ad21.chunk.js"
+  },
+  {
+    "revision": "6c87b3b834d3ab147f26",
+    "url": "/ilyas-code/ilyasFolder.git/static/js/runtime-main.85e52369.js"
+  }
+]);
