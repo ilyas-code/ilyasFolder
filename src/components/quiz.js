4c52e3aef5a175ca5a294f723da9913d11458d4f
ilyas-code <ilyas515253@gmail.com>
2020-10-23 19:28:36 +0530

    debug update

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,20 +1,64 @@
 const Quiz = [
   {
     qid: 0,
-    Question: "what is the shape of the earth",
-    Options: ["square", "sphere", "conical", "plane"],
-    Ans: "sphere"
+    Question: "Which one of the following river flows between Vindhyan and Satpura ranges?",
+    Options: ["Narmada", "Mahanadi", "Son", "Netravati"],
+    Ans: "Narmada"
   },
   {
     qid: 1,
-    Question: "Entomology is the science that studies",
+    Question: "The Central Rice Research Station is situated in",
     Options: [
-      "Behavior of human beings",
-      "Insects",
-      "The origin and history of technical and scientific terms",
-      "The formation of rocks"
+      "Chennai",
+      "Cuttack",
+      "Banglore",
+      "Quilon"
     ],
-    Ans: "Insects"
+    Ans: "Cuttack"
+  },
+  {
+    qid: 2,
+    Question: "Who among the following wrote Sanskrit grammar?",
+    Options: [
+      "Kalidasa",
+      "Charak",
+      "Panini",
+      "Aryabhatt"
+    ],
+    Ans: "Panini"
+  },
+  {
+    qid: 3,
+    Question: "Which among the following headstreams meets the Ganges in last?",
+    Options: [
+      "Alaknanda",
+      "Pindar",
+      "Mandakini",
+      "Bhagirathi"
+    ],
+    Ans: "Bhagirathi"
+  },
+  {
+    qid: 4,
+    Question: "The metal whose salts are sensitive to light is?",
+    Options: [
+      "Zinc",
+      "Silver",
+      "Copper",
+      "Aluminum"
+    ],
+    Ans: "Silver"
+  },
+  {
+    qid: 5,
+    Question: "The country that has the highest in Barley Production?",
+    Options: [
+      "China",
+      "India",
+      "Russia",
+      "France"
+    ],
+    Ans: "Russia"
   }
 ];
 
